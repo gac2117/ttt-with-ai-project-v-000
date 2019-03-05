d94ef5f8be2348c82720ae0a1aa2078a0b9a6f8b
Grace Choi <oiograceoio@gmail.com>
2019-03-05 22:26:07 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -11,10 +11,10 @@ class Game
   ]
   attr_accessor :board, :player_1, :player_2
 
-  def initialize(board ,player_1, player_2)
-    @board = board
+  def initialize(player_1, player_2, board)
     @player_1 = player_1
     @player_2 = player_2
+    @board = board
   end
 
   def current_player
